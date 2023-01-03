f3a34a962235e24121b361177f81ada9bd9ced04
iamop9211 <priyanshu.mcs@gmail.com>
2023-01-03 18:30:39 +0530

    vercel json file

--- main.py
+++ main.py
@@ -232,6 +232,6 @@ def delete_post(post_id):
     db.session.commit()
     return redirect(url_for('get_all_posts'))
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False)
+#
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', debug=False)
